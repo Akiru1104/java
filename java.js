e17412b17342adba80234d0ae506eb0b78b4c2ac
25LP5290 25LP5290 <zulaa.1104@gmail.com>
2025-10-28 16:15:59 +0800

    first commit

---
+++ java.js
@@ -0,0 +1,170 @@
+// const aaa = "aaa";
+// const bbb = 123;
+
+// const hours = 2;
+// console.log(hours * 60);
+
+// const hours = 12;
+// console.log(hours * 60);
+
+// const hours = 2.5;
+// console.log(hours * 60);
+
+// const number = 2;
+// console.log(2 % 2 === 0 && "even");
+// console.log(2 % 2 === 1 && "odd");
+
+// const tegsh = 7;
+// console.log(7 % 2 === 0 && "even");
+// console.log(7 % 2 === 1 && "odd");
+
+// const sondgoi = 1;
+// console.log(1 % 2 === 0 && "even");
+// console.log(1 % 2 === 1 && "odd");
+
+// let a = 1,
+//   b = 8,
+//   c = 3;
+
+// console.log(a < b && b < c && c);
+// console.log(b < c && c < a && a);
+// console.log(c < a && a < b && b);
+// console.log(c < b && b < a && a);
+
+// let q = 9;
+// w = 4;
+// e = 3;
+// console.log(q <= w && <= e && e);
+// console.log(w >= q && q >= q && q);
+
+// 10/28
+
+// #1
+
+// const hours = 38;
+// const honog = Math.floor(hours / 24);
+// console.log(honog);
+// const tsag = hours - honog * 24;
+// console.log(tsag);
+
+// ＃2
+
+// const days = 4;
+// console.log(days * 24);
+// const hours = 40;
+// const total = hours + days * 24;
+// console.log(total);
+
+// #3
+
+// const sar = 106;
+// const jil = Math.floor(sar / 12);
+// const uldsensar = sar % 12;
+// console.log(jil, "jil", uldsensar, "sar");
+
+// #4
+
+// const jil = 90;
+// const sar = 98;
+// const jileessar = Math.floor(jil / 12);
+// console.log(jileessar);
+// const uldsensar = jil % 12;
+// console.log(uldsensar);
+// console.log(jileessar, "jil", uldsensar, "sar");
+
+// // #5
+// const num1 = 9;
+// const num2 = 3;
+// const num3 = 2;
+// const num4 = 8;
+
+// let min = num1;
+
+// if (min > num2) {
+//   min = num2;
+// }
+// if (min > num3) {
+//   min = num3;
+// }
+// if (min > num4) {
+//   min = num4;
+// }
+// console.log(min);
+
+// // #6
+
+// const num1 = 2;
+// const num2 = 9;
+// const num3 = 4;
+// const num4 = 3;
+
+// let max = 1;
+
+// if (num1 < 5) {
+//   max = max * num1;
+// }
+
+// if (num2 < 5) {
+//   max = max * num2;
+// }
+// if (num3 < 5) {
+//   max = max * num3;
+// }
+// if (num4 < 5) {
+//   max = max * num4;
+// }
+// console.log(max);
+
+// //  #7
+
+// const num1 = 11;
+// const num2 = 9;
+// const num3 = 4;
+// const num4 = 3;
+
+// let total = 1;
+
+// if (num1 > 5) {
+//   total = total * num1;
+// }
+// if (num2 > 5) {
+//   total = total * num2;
+// }
+// if (num3 > 5) {
+//   total = total * num3;
+// }
+// if (num4 > 5) {
+//   total = total * num4;
+// }
+
+// console.log(total);
+
+//#1-1
+
+// const num1 = 20;
+// const num2 = 40;
+
+// if (num1 > num2) {
+//   console.log(num1 + "Ih too");
+// } else {
+//   console.log(num2 + "Ih too");
+// }
+
+// #1-2
+
+// const too = 11;
+
+// if (too/2){};
+// else {
+//     too
+// }
+
+// // #1-4
+
+// const text = "sodnomariunzul";
+// console.log(text.length);
+
+// #1-6
+
+const text = "sodnomariunzul";
+console.log(text.toUpperCase());
